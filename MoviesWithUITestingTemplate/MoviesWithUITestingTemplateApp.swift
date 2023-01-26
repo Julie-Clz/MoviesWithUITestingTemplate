e22f663c2f971dbbb15d785b38f87598697cd6f3
julie collazos <juliecollazos@hotmail.com>
2023-01-26 18:12:36 +0100

    Initial Commit

---
+++ MoviesWithUITestingTemplate/MoviesWithUITestingTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesWithUITestingTemplateApp.swift
+//  MoviesWithUITestingTemplate
+//
+//  Created by Julie Collazos on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesWithUITestingTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
